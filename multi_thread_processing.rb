f530fd7ee7c7a0ef0658598fc734aa03a03bb3f8
edikgat <edikgat@gmail.com>
2014-01-15 19:48:27 +0400

    multi thread processing

---
+++ multi_thread_processing.rb
@@ -0,0 +1,75 @@
+class MultiThreadProcessing
+  def initialize(thread_number = 5)
+    @thread_number = thread_number
+  end
+
+  def process(collection, &mapper)
+    @collection = collection
+    @mapper = mapper
+    process_collection
+  end
+
+  private
+
+  def process_collection
+    map_items
+    process_queue
+  end
+
+  def map_items
+    init_private_settings
+    Thread.new do
+      @collection.each do |item|
+        @thread_group_mutex.synchronize do
+          add_thread(item)
+          @resource.wait(@thread_group_mutex)
+        end
+      end
+    end
+  end
+
+  def init_private_settings
+    Thread.abort_on_exception = true
+    @thread_group_mutex = Mutex.new
+    @resource = ConditionVariable.new
+    @thread_group = ThreadGroup.new
+    @queue = Queue.new
+  end
+
+  def add_thread(item)
+    @thread_group.add(
+      Thread.new do
+        new_thread_event_if_it_needed            
+        process_item(item)
+        new_thread_event
+      end)
+  end
+
+  def new_thread_event
+    @thread_group_mutex.synchronize do
+      @resource.signal
+    end
+  end
+
+  def new_thread_event_if_it_needed
+    @thread_group_mutex.synchronize do
+      if @thread_group.list.length <= @thread_number
+        @resource.signal
+      end
+    end
+  end
+
+  def process_item(item)
+    processed_item = @mapper.call(item)
+    @queue << processed_item
+  end
+
+  def process_queue
+    arr = []
+    @collection.size.times do
+      arr << @queue.pop
+    end
+    arr
+  end
+
+end
